98ff989374b7ed932345efa212a3b918be9f8954
Alice Robinson <wi24989@bristol.ac.uk>
2026-02-25 10:22:28 +0000

    Finalising script 1 - ter list

--- scripts/1_Teratogenic_Drugs_of_Interest.R
+++ scripts/1_Teratogenic_Drugs_of_Interest.R
@@ -1,12 +1,37 @@
+##
+# Collating manually extracted files to create a list of teratogenic drugs
+# of interest
+##
+
 #######################################################
-# Set Working Directory
+# Load in libraries
 #######################################################
-install.packages("dotenv")
 
 library(dotenv)
+library(dplyr)
+
+#######################################################
+# Initialising file paths
+#######################################################
 
 load_dot_env("config.env")
 
-data_dir <- Sys.getenv("datadir")
-results_dir <- Sys.getenv("resultsdir")
+raw_data <- Sys.getenv("rawdatadir")
+interim_data <- Sys.getenv("interimdatadir")
 
+input_data <- file.path(raw_data, "drug_extracted_data")
+output_data <- file.path(interim_data, "teratogenic_drugs.txt")
+
+#######################################################
+# Creating list of Teratogenic drugs to look at
+#######################################################
+
+drug_list <- list.files(input_data, pattern = ".csv") %>%
+  as.list() %>%
+  gsub(pattern = ".csv$", replacement = "")
+
+#######################################################
+# Save
+#######################################################
+
+writeLines(drug_list, output_data)
